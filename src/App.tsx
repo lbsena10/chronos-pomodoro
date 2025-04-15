91aae288709be9ab7ee201bbe6e2a45510375c55
lbsena10 <lucas.lbsena@gmail.com>
2025-04-14 22:10:47 -0300

    add: input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -12,7 +14,32 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css';
+
+type DefaultInputPropos = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputPropos) {
+  return (
+    <>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input className={styles.input} id={id} type={type} {...rest} />
+    </>
+  );
+}
